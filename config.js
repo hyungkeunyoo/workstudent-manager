32dbff390cc938071a73562f8b9c1f5489fcbaff
hyungkeunyoo <hkyoo0810@gmail.com>
2026-08-29 22:52:20 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.WORK_CONFIG = {
+  SYSTEM_NAME: "근로장학생 근무관리",
+  TERM_NAME: "2026-2학기",
+  API_URL: "https://script.google.com/macros/s/AKfycbyNbKtdmQV5bnt_nqbgqYJV4zhKUvwOQmPvF3hWOFA-kTTQJWYnQkrAJqss6bucRIwK/exec",
+  DEMO_MODE: false
+};
