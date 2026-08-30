14c435c689e06b474231019db3066a8bc1c2376a
hyungkeunyoo <hkyoo0810@gmail.com>
2026-08-30 16:49:32 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.WORK_CONFIG = {
   SYSTEM_NAME: "근로장학생 근무관리",
   TERM_NAME: "2026-2학기",
   API_URL: "https://script.google.com/macros/s/AKfycbyNbKtdmQV5bnt_nqbgqYJV4zhKUvwOQmPvF3hWOFA-kTTQJWYnQkrAJqss6bucRIwK/exec",
-  PUBLIC_FEED_SHEET_ID: "", // setupPublicCalendarFeed() 실행 후 생성된 공개 피드 스프레드시트 ID
+  PUBLIC_FEED_SHEET_ID: "1cXNx-asYKU_KqIMQPRy7rPDZvENjcjmOPmAGsnn98X4", // setupPublicCalendarFeed() 실행 후 생성된 공개 피드 스프레드시트 ID
   DEMO_MODE: false
 };
